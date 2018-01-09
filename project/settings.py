b513f99604e2956d0871a2014d516e64714abe65
Patrick Sandiford <Patrick.Sandiford@cox.com>
2018-01-09 16:59:42 -0500

    removing welcome app

--- project/settings.py
+++ project/settings.py
@@ -67,7 +67,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'debug_toolbar',
-    'welcome',
     'jquery',
     'bootstrap3',
     'swift_browser',
--- project/urls.py
+++ project/urls.py
@@ -3,14 +3,12 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
-from welcome.views import index, health
 from swift_browser.views import container, containers, create_container, delete_container, upload, delete_object, create_folder
 
 urlpatterns = [
     url(r'^$', containers, name='containers'),
     url(r'^accounts/login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': '/accounts/login'}, name='logout'),
-    url(r'^health$', health),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^create_container/$', create_container, name='create_container'),
     url(r'^delete_container/$', delete_container, name='delete_container'),
