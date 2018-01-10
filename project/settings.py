8a578b6d6aaa044848f891f033e226e96a9bbff7
Patrick Sandiford <Patrick.Sandiford@cox.com>
2018-01-10 03:48:19 -0500

    Added LDAP integration

--- project/settings.py
+++ project/settings.py
@@ -54,6 +54,11 @@ LOGGING = {
             'level': LOGGING_LEVEL,
 	    'propagate': True,
         },
+        'django_python3_ldap': {
+            'handlers': ['console'],
+            'level': LOGGING_LEVEL,
+	    'propagate': True,
+        },
     },
 }
 
@@ -67,6 +72,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'debug_toolbar',
+    'django_python3_ldap',
     'jquery',
     'bootstrap3',
     'swift_browser',
@@ -170,3 +176,85 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 INTERNAL_IPS = ['127.0.0.1']
 
 LOGIN_REDIRECT_URL = 'containers'
+
+AUTHENTICATION_BACKENDS = [
+    'django_python3_ldap.auth.LDAPBackend',
+    'django.contrib.auth.backends.ModelBackend',
+]
+
+# The URL of the LDAP server.
+LDAP_AUTH_URL = os.environ.get('LDAP_AUTH_URL',
+        'ldap://localhost:389')
+
+# Initiate TLS on connection.
+LDAP_AUTH_USE_TLS = os.environ.get('LDAP_AUTH_USE_TLS',
+	False)
+
+# The LDAP search base for looking up users.
+LDAP_AUTH_SEARCH_BASE = os.environ.get('LDAP_AUTH_SEARCH_BASE',
+        'ou=people,dc=example,dc=com')
+
+# The LDAP class that represents a user.
+LDAP_AUTH_OBJECT_CLASS = os.environ.get('LDAP_AUTH_OBJECT_CLASS',
+        'inetOrgPerson')
+
+# User model fields mapped to the LDAP
+# attributes that represent them.
+LDAP_AUTH_USER_FIELDS = {
+    "username": os.environ.get('LDAP_AUTH_USERNAME_ATTR', 'uid'),
+    "first_name": os.environ.get('LDAP_AUTH_FIRSTNAME_ATTR', 'givenName'),
+    "last_name": os.environ.get('LDAP_AUTH_LASTNAME_ATTR', 'sn'),
+    "email": os.environ.get('LDAP_AUTH_EMAIL_ATTR', 'mail'),
+}
+
+# A tuple of django model fields used to uniquely identify a user.
+LDAP_AUTH_USER_LOOKUP_FIELDS = os.environ.get('LDAP_AUTH_USER_LOOKUP_FIELDS',
+        ("username",))
+
+# Path to a callable that takes a dict of {model_field_name: value},
+# returning a dict of clean model data.
+# Use this to customize how data loaded from LDAP is saved to the User model.
+LDAP_AUTH_CLEAN_USER_DATA = os.environ.get('LDAP_AUTH_CLEAN_USER_DATA',
+        "django_python3_ldap.utils.clean_user_data")
+
+# Path to a callable that takes a user model and a dict of {ldap_field_name: [value]},
+# and saves any additional user relationships based on the LDAP data.
+# Use this to customize how data loaded from LDAP is saved to User model relations.
+# For customizing non-related User model fields, use LDAP_AUTH_CLEAN_USER_DATA.
+LDAP_AUTH_SYNC_USER_RELATIONS = os.environ.get('LDAP_AUTH_SYNC_USER_RELATIONS',
+        "django_python3_ldap.utils.sync_user_relations")
+
+# Path to a callable that takes a dict of {ldap_field_name: value},
+# returning a list of [ldap_search_filter]. The search filters will then be AND'd
+# together when creating the final search filter.
+LDAP_AUTH_FORMAT_SEARCH_FILTERS = os.environ.get('LDAP_AUTH_FORMAT_SEARCH_FILTERS',
+        "django_python3_ldap.utils.format_search_filters")
+
+# Path to a callable that takes a dict of {model_field_name: value}, and returns
+# a string of the username to bind to the LDAP server.
+# Use this to support different types of LDAP server.
+LDAP_AUTH_FORMAT_USERNAME = os.environ.get('LDAP_AUTH_FORMAT_USERNAME',
+        'django_python3_ldap.utils.format_username_openldap')
+
+# Sets the login domain for Active Directory users.
+LDAP_AUTH_ACTIVE_DIRECTORY_DOMAIN = os.environ.get('LDAP_AUTH_ACTIVE_DIRECTORY_DOMAIN',
+        None)
+
+# The LDAP username and password of a user for querying the LDAP database for user
+# details. If None, then the authenticated user will be used for querying, and
+# the `ldap_sync_users` command will perform an anonymous query.
+LDAP_AUTH_CONNECTION_USERNAME = os.environ.get('LDAP_AUTH_CONNECTION_USERNAME',
+        None)
+LDAP_AUTH_CONNECTION_PASSWORD = os.environ.get('LDAP_AUTH_CONNECTION_PASSWORD',
+        None)
+
+# Set connection/receive timeouts (in seconds) on the underlying `ldap3` library.
+LDAP_AUTH_CONNECT_TIMEOUT = os.environ.get('LDAP_AUTH_CONNECT_TIMEOUT',
+        None)
+LDAP_AUTH_RECEIVE_TIMEOUT = os.environ.get('LDAP_AUTH_RECEIVE_TIMEOUT',
+        None)
+
+# Users must be a member of this group to access
+LDAP_GROUP_MEMBER = os.environ.get('LDAP_GROUP_MEMBER',
+        None)
+
